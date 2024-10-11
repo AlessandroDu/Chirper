7688b36e62cecc7095db4478c2d39bb9d59c0e3e
Alessandro <duccialessandro001@gmail.com>
2024-10-11 09:55:17 +0400

    AUTH: BREEZE, INDEX: Installed Breeze module with blade for auth features. Created the index route, controller method and view with form to create chirps.

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Response;
 use App\Models\Chirp;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
@@ -13,7 +14,7 @@ class ChirpController extends Controller
      */
     public function index()
     {
-        //
+        return view('chirps.index');
     }
 
     /**
